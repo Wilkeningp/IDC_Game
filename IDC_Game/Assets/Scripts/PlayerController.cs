e8d454063bf9b03d8d0a85fa0afb70c07dea6fcf
Patrick Wilkening <wilkeningp@gmail.com>
2017-03-25 13:48:22 -0700

    made a prefabs folder for game objects.

--- IDC_Game/Assets/Scripts/PlayerController.cs
+++ IDC_Game/Assets/Scripts/PlayerController.cs
@@ -13,12 +13,27 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     public Boundary boundary;
 
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void FixedUpdate()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        rb.velocity = movement * speed;
+
+        rb.position = new Vector2
+            (
+                Mathf.Clamp (rb.position.x, boundary.xMin, boundary.xMax),
+                Mathf.Clamp (rb.position.y, boundary.yMin, boundary.yMax)
+            );
         
     }
 }
