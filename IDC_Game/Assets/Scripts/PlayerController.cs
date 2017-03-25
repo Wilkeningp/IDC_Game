b6a202c00116063171b886c3a024d817dd1ae86f
Patrick Wilkening <wilkeningp@gmail.com>
2017-03-25 13:23:23 -0700

    Basic things so far

---
+++ IDC_Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Boundary boundary;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        
+    }
+}
