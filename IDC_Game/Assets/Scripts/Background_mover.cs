0913af2b951fcbfa9ad07558532596c8dcb6de3d
Patrick Wilkening <wilkeningp@gmail.com>
2017-04-08 02:42:07 -0700

    Getting some art into the game now. Added an updated background that scrolls.

---
+++ IDC_Game/Assets/Scripts/Background_mover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_mover : MonoBehaviour {
+
+    public float scrollSpeed;
+    public float titleSize;
+    
+    private Vector2 startPosition;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, titleSize);
+        transform.position = startPosition + Vector2.down * newPosition;
+
+	}
+}
---
+++ IDC_Game/Assets/Scripts/Background_painter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background_painter : MonoBehaviour {
+
+    public Color lightWorld;
+    public Color darkWorld;
+
+    private Dimension current;
+    private GameObject gm;
+    private SpriteRenderer rend;
+
+    // Use this for initialization
+    void Start () {
+        gm = GameObject.Find("GameManager");
+        current = gm.GetComponent<GameManager>().getDimension();
+        rend = GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        current = gm.GetComponent<GameManager>().getDimension();
+
+        if (current == Dimension.Light)
+        {
+            rend.color = lightWorld;
+        }
+        else
+        {
+            rend.color = darkWorld;
+        }
+    }
+}
